1548466f96a08d40a73ddb3f5f3e022a639bb49b
Manuel Huber <ManuelHu@users.noreply.github.com>
2025-07-01 17:31:08 +0200

    docs: fix deprecation warnings

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,16 +1,15 @@
 # Configuration file for the Sphinx documentation builder.
 from __future__ import annotations
 
+import importlib.metadata
 import sys
 from pathlib import Path
-
-from pkg_resources import get_distribution
 
 sys.path.insert(0, Path(__file__).parents[2].resolve().as_posix())
 
 project = "l200geom"
 copyright = "Copyright Holder"
-version = get_distribution("legend-pygeom-l200").version
+version = importlib.metadata.version("legend-pygeom-l200")
 
 extensions = [
     "sphinx.ext.githubpages",
@@ -55,7 +54,7 @@ napoleon_use_ivar = True
 intersphinx_mapping = {
     "python": ("https://docs.python.org/3", None),
     "numpy": ("https://numpy.org/doc/stable", None),
-    "scipy": ("https://docs.scipy.org/doc/scipy/reference", None),
+    "scipy": ("https://docs.scipy.org/doc/scipy", None),
     "pandas": ("https://pandas.pydata.org/docs", None),
     "matplotlib": ("https://matplotlib.org/stable", None),
     "pint": ("https://pint.readthedocs.io/en/stable", None),
