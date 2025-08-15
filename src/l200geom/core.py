b049b5731f6b20455f5ee1973dfac4b1a6724aea
Manuel Huber <info@manuelhu.de>
2025-08-15 18:13:03 +0200

    use lazy=True for LegendMetadata

--- src/l200geom/core.py
+++ src/l200geom/core.py
@@ -66,7 +66,7 @@ def construct(
     lmeta = None
     if not public_geometry:
         with contextlib.suppress(GitCommandError):
-            lmeta = LegendMetadata()
+            lmeta = LegendMetadata(lazy=True)
     # require user action to construct a testdata-only geometry (i.e. to avoid accidental creation of "wrong"
     # geometries by LEGEND members).
     if lmeta is None and not public_geometry:
